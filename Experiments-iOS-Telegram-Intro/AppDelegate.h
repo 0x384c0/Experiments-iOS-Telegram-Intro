2686cd0a1bddf03da9e36c4d612f9069ef9c28c9
Andrew <andrew.ashurov@spalmalo.com>
2017-12-27 17:44:33 +0600

    initial commit

---
+++ Experiments-iOS-Telegram-Intro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Experiments-iOS-Telegram-Intro
+//
+//  Created by 0x384c0 on 12/27/17.
+//  Copyright © 2017 Spalmalo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Readme.md
@@ -0,0 +1,3 @@
+Intro screen from telegram ios app
+
+![screenshot](screenshots/screenshot.png)
